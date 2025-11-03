31da504514d4e90835b2b6bd05a88b8dcc418bb9
Ridho Syahbana Putra <ridhosyahbanaputra@gmail.com>
2025-11-03 12:39:25 +0700

    Terdapat masalah pada subscribe, unsubscribe, dan notif tidak mau muncul

---
+++ src/scripts/data/api-mapper.js
@@ -0,0 +1,15 @@
+import Map from '../utils/map';
+
+export async function reportMapper(report) {
+  return {
+    ...report,
+    location: {
+      ...report.location,
+      placeName: await Map.getPlaceNameByCoordinate(
+        report.location.latitude,
+        report,
+        location.longitude,
+      ),
+    },
+  };
+}
